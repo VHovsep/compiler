7b243b944f8301ae7f60301acb4e8cf661693eb3
Hovsep Vardanyan <33977944+VHovsep@users.noreply.github.com>
2017-11-26 21:01:12 +0400

    Update lex-pars.cpp

--- lex-pars.cpp
+++ lex-pars.cpp
@@ -7,6 +7,11 @@
 
 using namespace std;
 
+operator_node::operator_node(exp_node *L, exp_node *R) {
+        left    = L;
+        right   = R;
+}
+
 number_node::number_node(float value) {
         num = value;
 }
@@ -28,6 +33,222 @@ float number_node::evaluate() {
 float id_node::evaluate() {
         cout << "id_node: " << id << " = " << idTable[id] << endl;
         return idTable[id];
+}
+
+plus_node::plus_node(exp_node *L, exp_node *R) : operator_node(L,R) {
+}
+
+void plus_node::print() {
+        cout << "(";
+        left->print();
+        cout << " + ";
+        right->print();
+        cout << ")";
+}
+
+float plus_node::evaluate() {
+        float left_num, right_num;
+
+        left_num  = left->evaluate();
+        right_num = right->evaluate();
+
+        num = left_num + right_num;
+        cout << "plus_node: " << left_num << " + " << right_num << " = " << num << "\n";
+        return (num);
+}
+
+times_node::times_node(exp_node *L, exp_node *R) : operator_node(L,R) {
+}
+
+void times_node:: print() {
+        cout << "(";
+        left->print();
+        cout << " * ";
+        right->print();
+        cout << ")";
+}
+
+float times_node::evaluate() {
+        float left_num, right_num;
+
+        left_num = left->evaluate();
+        right_num = right->evaluate();
+
+        num = left_num * right_num;
+        cout << "times_node: " << left_num << " * " << right_num << " = " << num << "\n";
+        return (num);
+}
+
+minus_node::minus_node(exp_node *L, exp_node *R) : operator_node(L, R) {
+}
+
+void minus_node::print(){
+        cout<<"(";
+        left->print();
+        cout<<"-";
+        right->print();
+        cout<<")";
+}
+
+float minus_node::evaluate(){
+        float left_num,right_num;
+
+        left_num = left->evaluate();
+        right_num = right->evaluate();
+
+        num = left_num - right_num;
+        cout<<"minus_node: "<<left_num<<"-"<<right_num<<"="<<num<<"\n";
+
+        return (num);
+}
+
+divided_node::divided_node(exp_node *L, exp_node *R) : operator_node(L, R){
+}
+
+void divided_node::print(){
+        cout<<"(";
+        left->print();
+        cout<<"/";
+        right->print();
+        cout<<")";
+}
+
+float divided_node::evaluate(){
+        float left_num,right_num;
+
+        left_num = left->evaluate();
+        right_num = right->evaluate();
+
+        num = left_num / right_num;
+
+        cout<<"divided_node: "<<left_num<<"/"<<right_num<<"="<<num<<"\n";
+
+        return (num);
+}
+
+small_node::small_node(string left, string right)
+        :id_1(left)
+        ,id_2(right)
+{
+
+}
+
+void small_node::print() {
+        cout<<"(";
+        cout<<id_1;
+        cout<<" < ";
+        cout<<id_2;
+        cout<<")";
+}
+
+
+float small_node::evaluate() {
+        float left_num,right_num;
+
+        left_num = idTable[id_1];
+        right_num = idTable[id_2];
+
+        result = (left_num < right_num);
+
+        if(result == 1)
+          cout<<"small_node: "<<id_1<<" < "<<id_2<<" "<<"true"<<endl;
+        else
+          cout<<"small_node: "<<id_1<<" < "<<id_2<<" "<<"false"<<endl;
+
+        return (result);
+}
+
+great_node::great_node(string left, string right)
+        :id_1(left)
+        ,id_2(right)
+{
+
+}
+
+void great_node::print() {
+        cout<<"(";
+        cout<<id_1;
+        cout<<" > ";
+        cout<<id_2;
+        cout<<")";
+}
+
+float great_node::evaluate() {
+        float left_num, right_num;
+
+        left_num = idTable[id_1];
+        right_num = idTable[id_2];
+
+        result = (left_num > right_num);
+
+        if(result == 1)
+          cout<<"great_node: "<<id_1<<" > "<<id_2<<" "<<"true"<<endl;
+        else
+          cout<<"great_node: "<<id_1<<" > "<<id_2<<" "<<"false"<<endl;
+
+        return (result);
+}
+
+logical_or_node::logical_or_node(string left, string right)
+        :id_1(left)
+        ,id_2(right)
+{
+
+}
+
+void logical_or_node::print() {
+        cout<<"(";
+        cout<<id_1;
+        cout<<" || ";
+        cout<<id_2;
+        cout<<")";
+}
+
+float logical_or_node::evaluate() {
+        float left_num, right_num;
+
+        left_num = idTable[id_1];
+        right_num = idTable[id_2];
+
+        result = (left_num || right_num);
+
+        if(result == 1)
+          cout<<"logical_or_node: "<<id_1<<" || "<<id_2<<" "<<"true"<<endl;
+        else
+          cout<<"logical_or_node: "<<id_1<<" || "<<id_2<<" "<<"false"<<endl;
+
+        return (result);
+}
+
+logical_or_node::logical_or_node(string left, string right)
+        :id_1(left)
+        ,id_2(right)
+{
+
+}
+
+void logical_or_node::print() {
+        cout<<"(";
+        cout<<id_1;
+        cout<<" || ";
+        cout<<id_2;
+        cout<<")";
+}
+
+float logical_or_node::evaluate() {
+        float left_num, right_num;
+
+        left_num = idTable[id_1];
+        right_num = idTable[id_2];
+
+        result = (left_num || right_num);
+
+        if(result == 1)
+          cout<<"logical_or_node: "<<id_1<<" || "<<id_2<<" "<<"true"<<endl;
+        else
+          cout<<"logical_or_node: "<<id_1<<" || "<<id_2<<" "<<"false"<<endl;
+
+        return (result);
 }
 
 assignment_stmt::assignment_stmt(string name, exp_node *expression)
